b10c2c68995c3d8f7128fa74290ce805a160f5e6
egalitarian11 <seanhallinan88@gmail.com>
2019-12-02 12:45:33 -0500

    Clean up hello component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { PatientDataService } from './services/patient-data.service';
 import { PatientComponent } from './patient-component/patient-component.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, PatientComponent ],
+  declarations: [ AppComponent, PatientComponent ],
   bootstrap:    [ AppComponent ],
   providers: [PatientDataService]
 })
